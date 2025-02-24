6b5bffb20b9ed98f04d0a9920bca582e192bcd69
matheushssilvaa <msxatendimento@outlook.com.br>
2025-02-24 07:28:52 -0300

    feat: add product header component

---
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Product } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface ProductHeaderProps {
+    product: Pick<Product, 'name' | 'imageUrl'>
+}
+
+const ProductHeader = ({ product }: ProductHeaderProps) => {
+    const router = useRouter();
+    const handleBackClick = () => router.back();
+    return (
+        <>
+            <Button
+                variant="secondary"
+                size="icon"
+                className="absolute top-4 left-4 rounded-full z-50"
+                onClick={handleBackClick}
+            >
+                <ChevronLeftIcon />
+            </Button>
+            <Button
+                variant="secondary"
+                size="icon"
+                className="absolute top-4 right-4 rounded-full z-50">
+                <ScrollTextIcon />
+            </Button>
+            <Image
+                src={product.imageUrl}
+                alt={product.name}
+                fill
+                className="object-contain"
+            />
+        </>
+    )
+}
+
+export default ProductHeader;
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,16 +1,18 @@
 import { Product } from "@prisma/client";
 import Link from "next/link";
 import Image from "next/image";
+import { useParams } from "next/navigation";
 
 interface ProductProps {
     products: Product[]
 }
 
 const Products = ({ products }: ProductProps) => {
+    const { slug } = useParams<{ slug: string }>()
     return (
         <div className="space-y-3 px-5">
             {products.map((product) => (
-                <Link key={product.id} href="/" className="flex items-center justify-between gap-10 border-b py-3">
+                <Link key={product.id} href={`/${slug}/menu/${product.id}`} className="flex items-center justify-between gap-10 border-b py-3">
                     <div>
                         <h3 className="text-sm font-medium">{product.name}</h3>
                         <p className="line-clamp-2 text-sm text-muted-foreground">
